497dc032c541384d83df6ac23ec376af0863dba1
Michael Stonis <120685+michaelstonis@users.noreply.github.com>
2023-11-27 22:40:51 -0600

    Initial commit

---
+++ MauiDrawer/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace MauiDrawer;
+
+// All the code in this file is only included on Mac Catalyst.
+public class PlatformClass1
+{
+}
+
---
+++ MauiDrawer/Platforms/Tizen/PlatformClass1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MauiDrawer
+{
+	// All the code in this file is only included on Tizen.
+	public class PlatformClass1
+	{
+	}
+}
